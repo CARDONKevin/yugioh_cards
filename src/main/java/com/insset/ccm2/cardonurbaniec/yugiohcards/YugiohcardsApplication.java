c53bb6f3178cecc60552448d3ee6e8bc05682cdc
CARDON <cardonkevin02@gmail.com>
2019-10-08 22:40:05 +0200

    Initial commit

---
+++ src/main/java/com/insset/ccm2/cardonurbaniec/yugiohcards/YugiohcardsApplication.java
@@ -0,0 +1,13 @@
+package com.insset.ccm2.cardonurbaniec.yugiohcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YugiohcardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YugiohcardsApplication.class, args);
+	}
+
+}
