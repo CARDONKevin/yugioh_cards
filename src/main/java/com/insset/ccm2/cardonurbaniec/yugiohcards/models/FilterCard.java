e03514762aaf78122dbfc0497ccdae42bb54fb33
CARDON <cardonkevin02@gmail.com>
2020-01-26 15:35:39 +0100

    add getter and setter filtercard to serialize and deserialize

--- src/main/java/com/insset/ccm2/cardonurbaniec/yugiohcards/models/FilterCard.java
+++ src/main/java/com/insset/ccm2/cardonurbaniec/yugiohcards/models/FilterCard.java
@@ -26,4 +26,84 @@ public class FilterCard {
     private int def;
 
     private int level;
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+
+    public CardSubType getCardSubType() {
+        return cardSubType;
+    }
+
+    public void setCardSubType(CardSubType cardSubType) {
+        this.cardSubType = cardSubType;
+    }
+
+    public CategorizedType getCategorized() {
+        return categorized;
+    }
+
+    public void setCategorized(CategorizedType categorized) {
+        this.categorized = categorized;
+    }
+
+    public AttributeType getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(AttributeType attribute) {
+        this.attribute = attribute;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
